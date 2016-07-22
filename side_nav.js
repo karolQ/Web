d719d0aa517f87f5c7f85d7635c20ea1458b6c10
zhaohe <zhaohezzu@gmail.com>
2016-07-22 14:11:41 -0400

    fix css transition problem

--- side_nav.js
+++ side_nav.js
@@ -1,19 +1,24 @@
 function myFunction(x){
 	x.classList.toggle("change");
 	document.getElementById("mySidenav").classList.toggle("show");
+//document.getElementById("mySidenav").style.width = "250px";
 	document.getElementById("main").classList.toggle("push");
 }
 
   window.onclick = function(event){
 	if(!event.target.matches('.menu_btn')){
-		var sidenavs = document.getElementByClassName('sidenav');
-		var i;
-		for(i=0;i<sidenavs.length;i++){
+		var sidenavs = document.getElementsByClassName('sidenav');
+		for(var i=0;i<sidenavs.length;i++){
 			var showSide = sidenavs[i];
 			if(showSide.classList.contains("show")){
 				showSide.classList.remove("show");
 			}
 		}
+
+		// var menu_btn = document.getElementById("menu_btn");
+		// if (menu_btn.classList.contains("change")) {
+		// 	menu_btn.classList.toggele("change");
+		// }
 
 		var mainBody = document.getElementById("main");
 		var j;
