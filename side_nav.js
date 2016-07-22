caeb738a8bac81f926350de8f7d4a02901ecaf28
Karol <yingkunli512@gmail.com>
2016-07-21 23:56:39 -0400

    side nav bar

---
+++ side_nav.js
@@ -0,0 +1,27 @@
+function myFunction(x){
+	x.classList.toggle("change");
+	document.getElementById("mySidenav").classList.toggle("show");
+	document.getElementById("main").classList.toggle("push");
+}
+
+  window.onclick = function(event){
+	if(!event.target.matches('.menu_btn')){
+		var sidenavs = document.getElementByClassName('sidenav');
+		var i;
+		for(i=0;i<sidenavs.length;i++){
+			var showSide = sidenavs[i];
+			if(showSide.classList.contains("show")){
+				showSide.classList.remove("show");
+			}
+		}
+
+		var mainBody = document.getElementById("main");
+		var j;
+		for(j=0; j< mainBody.length;j++){
+			var ele = mainBody[j];
+			if(ele.classList.contains("push")){
+				ele.classList.remove("push");ß
+			}
+		}
+	}
+}
