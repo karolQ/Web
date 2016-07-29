cb2df4e333b55891f2fa941e12ab0b7e35ced880
zhaohe <zhaohezzu@gmail.com>
2016-07-29 13:52:30 -0400

    change on side_nav.js

--- side_nav.js
+++ side_nav.js
@@ -5,28 +5,28 @@ function myFunction(x){
 	document.getElementById("main").classList.toggle("push");
 }
 
-  window.onclick = function(event){
-	if(!event.target.matches('.menu_btn')){
-		var sidenavs = document.getElementsByClassName('sidenav');
-		for(var i=0;i<sidenavs.length;i++){
-			var showSide = sidenavs[i];
-			if(showSide.classList.contains("show")){
-				showSide.classList.remove("show");
-			}
-		}
-
-		// var menu_btn = document.getElementById("menu_btn");
-		// if (menu_btn.classList.contains("change")) {
-		// 	menu_btn.classList.toggele("change");
-		// }
-
-		var mainBody = document.getElementById("main");
-		var j;
-		for(j=0; j< mainBody.length;j++){
-			var ele = mainBody[j];
-			if(ele.classList.contains("push")){
-				ele.classList.remove("push");
-			}
-		}
-	}
-}
+//   window.onclick = function(event){
+// 	if(!event.target.matches('.menu_btn')){
+// 		var sidenavs = document.getElementsByClassName('sidenav');
+// 		for(var i=0;i<sidenavs.length;i++){
+// 			var showSide = sidenavs[i];
+// 			if(showSide.classList.contains("show")){
+// 				showSide.classList.remove("show");
+// 			}
+// 		}
+//
+// 		// var menu_btn = document.getElementById("menu_btn");
+// 		// if (menu_btn.classList.contains("change")) {
+// 		// 	menu_btn.classList.toggele("change");
+// 		// }
+//
+// 		var mainBody = document.getElementById("main");
+// 		var j;
+// 		for(j=0; j< mainBody.length;j++){
+// 			var ele = mainBody[j];
+// 			if(ele.classList.contains("push")){
+// 				ele.classList.remove("push");
+// 			}
+// 		}
+// 	}
+// }
